60fedae719dcab54823d83b3ebb84a4a15b044d7
niguns7 <nirja767@gmail.com>
2022-11-09 20:26:42 +0545

    project started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './Components/Containers/Hero/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+export default App;
