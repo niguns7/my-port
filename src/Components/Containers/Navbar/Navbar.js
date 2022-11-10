9ccf5519ba78679e340b0ff82602d57676e89306
niguns7 <nirja767@gmail.com>
2022-11-10 16:25:02 +0545

    created and styled navbar

--- src/Components/Containers/Navbar/Navbar.js
+++ src/Components/Containers/Navbar/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
-//import classes from "./Navbar.module.css";
+import  "./Navbar.css";
+import {AiOutlineHome} from "react-icons/ai";
+import {AiOutlineUser} from "react-icons/ai";
+import {BiBook} from "react-icons/bi";
+import {RiServiceLine } from "react-icons/ri";
+import {BiMessageSquareEdit} from "react-icons/bi";
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <a href='#home' className='active'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experiences'><BiBook /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><BiMessageSquareEdit /></a>
+    </nav>
   )
 }
 
