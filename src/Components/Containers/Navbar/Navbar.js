4f93d2a829c2021609455ac75ceb61cc8087efe3
niguns7 <nirja767@gmail.com>
2022-11-10 16:40:50 +0545

    set active navbar components

--- src/Components/Containers/Navbar/Navbar.js
+++ src/Components/Containers/Navbar/Navbar.js
@@ -5,16 +5,36 @@ import {AiOutlineUser} from "react-icons/ai";
 import {BiBook} from "react-icons/bi";
 import {RiServiceLine } from "react-icons/ri";
 import {BiMessageSquareEdit} from "react-icons/bi";
+import { useState } from 'react';
 
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState ('#')
   return (
     <nav>
-      <a href='#home' className='active'><AiOutlineHome /></a>
-      <a href='#about'><AiOutlineUser /></a>
-      <a href='#experiences'><BiBook /></a>
-      <a href='#services'><RiServiceLine /></a>
-      <a href='#contact'><BiMessageSquareEdit /></a>
+      <a href='#home'
+      onClick={() => setActiveNav('#')}
+       className={activeNav === '#' ? 'active' : ''}>
+        <AiOutlineHome />
+        </a>
+
+      <a href='#about'
+       onClick={() => setActiveNav('#about')}
+       className={activeNav === '#' ? 'active' : ''}>
+        <AiOutlineUser />
+        </a>
+
+      <a href='#experiences'>
+        <BiBook />
+      </a>
+
+      <a href='#services'>
+      <RiServiceLine />
+      </a>
+
+      <a href='#contact'>
+      <BiMessageSquareEdit />
+      </a>
     </nav>
   )
 }
